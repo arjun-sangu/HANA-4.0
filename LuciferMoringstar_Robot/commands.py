7a7ff1406ff9e15d3b6a9e5828f6adcbfcb5ba2d
KOSHI Dev <86915159+arjun-sangu@users.noreply.github.com>
2022-06-18 15:32:53 +0530

    Update commands.py

--- LuciferMoringstar_Robot/commands.py
+++ LuciferMoringstar_Robot/commands.py
@@ -80,10 +80,19 @@ async def start(bot: lucifermoringstar_robot, update):
         return
 
     if len(update.command) != 2:
-        pr0fess0r_99 = [[ InlineKeyboardButton("× 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 ×", url=f"http://t.me/{temp.Bot_Username}?startgroup=true") ],
-                        [ InlineKeyboardButton("𝚂𝚄𝙿𝙿𝙾𝚁𝚃 💬", url=f"t.me/{SUPPORT}"), InlineKeyboardButton("𝚄𝙿𝙳𝙰𝚃𝙴𝚂 📢", url="t.me/Mo_Tech_YT") ],
-                        [ InlineKeyboardButton("ℹ️ 𝙷𝙴𝙻𝙿", callback_data="help"), InlineKeyboardButton("𝙰𝙱𝙾𝚄𝚃 🤠", callback_data="about") ]] 
-        await bot.send_photo(photo=random.choice(PICS), chat_id=update.chat.id, caption=START_MESSAGE.format(mention=update.from_user.mention, name=temp.Bot_Name, username=temp.Bot_Username), reply_markup=InlineKeyboardMarkup(pr0fess0r_99))
+        buttons = [[
+         InlineKeyboardButton("💢 Mʟᴍ Mᴏᴠɪᴇ Nɪɢʜᴛ 💢", url="https://t.me/NEW_MLM_HD_MOVES")
+         ],[
+         InlineKeyboardButton("❗ ʜᴇʟᴘ", callback_data="help"),
+         InlineKeyboardButton("🙂 ᴀʙᴏᴜᴛ", callback_data="about") 
+         ],[
+         InlineKeyboardButton("🍁 sʜᴀʀᴇ ᴍᴇ", url="https://telegram.me/share/url?url=https://t.me/MissHana_Msbot"),
+         InlineKeyboardButton("ᴄʟᴏsᴇ ✘", callback_data="close")
+         ]]
+        await bot.reply_photo(
+            photo=random.choice(FORCES), 
+            caption=START_MESSAGE.format(mention=update.from_user.mention, name=temp.Bot_Name, username=temp.Bot_Username),
+            reply_markup=InlineKeyboardMarkup(buttons))
 
 @lucifermoringstar_robot.on_message(filters.command(["admin", "admins"]) & filters.private, group=2)
 async def admin(bot: lucifermoringstar_robot, update):
